1a1638dec637ee878c2075e17d4113bdd61746ab
Hitendra singh <hitendrasingh5489@gmail.com>
2025-09-27 13:38:46 +0530

    feat: implement MongoDB connection and update environment variables

--- p6/src/index.js
+++ p6/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import  app  from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
     path: "./.env"
@@ -7,6 +8,12 @@ dotenv.config({
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
-});
+connectDB()
+.then (() => {
+  app.listen(PORT, () => {
+    console.log(`✅ Server running at http://localhost:${PORT}`);
+  })
+})
+.catch((err) => {
+  console.log("Mongodb connection error ", err)
+})
