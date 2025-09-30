e5aa3a1458ba82c913014e13535b8a385870e43c
Hitendra singh <hitendrasingh5489@gmail.com>
2025-09-30 15:26:31 +0530

    n

--- p6/src/models/playlist.models.js
+++ p6/src/models/playlist.models.js
@@ -1,8 +1,27 @@
 import mongoose, { Schema } from "mongoose"; // mongodb
 
 
-const videoSchema = new Schema({
-  videoFile: {
+const playlistSchema = new Schema({
+  name: {
     type: String,
     required: true,
   },
+  description: {
+    type: String,
+    required: true,
+  },
+  videos: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+},
+{ timestamps: true}
+);
+
+export const Playlist = mongoose.model("Playlist",playlistSchema);
---
+++ p6/src/models/subscription.models.js
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose"; // mongodb
+
+
+const subscriptionSchema = new Schema(
+  {
+    susbcriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const subscription = mongoose.model("Subscription",subscriptionSchemaSchema);
