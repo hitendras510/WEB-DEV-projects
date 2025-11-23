d2cddd74134201cb9119c4e4012e3a0acd348373
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-23 17:14:11 +0530

    new

--- TicTacToe - Game/script.js
+++ TicTacToe - Game/script.js
@@ -5,12 +5,12 @@ let gameover = new Audio("gameover.mp3");
 let turn = "X";
 let isgameover = false;
 
-//func to change the turn
+// Function to change the turn
 const changeTurn = () => {
   return turn === "X" ? "0" : "X";
 };
 
-//func to check for a win
+// Function to check for a win
 const checkWin = () => {
   let boxtext = document.getElementsByClassName("boxtext");
   let wins = [
@@ -41,8 +41,37 @@ const checkWin = () => {
       document.querySelector(".line").style.width = "20vw";
     }
   });
-}
+};
 
-//game logic 
-//music.play()
-let 
+// Game Logic
+// music.play()
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach((element) => {
+  let boxtext = element.querySelector(".boxtext");
+  element.addEventListener("click", () => {
+    if (boxtext.innerText === "") {
+      boxtext.innerText = turn;
+      turn = changeTurn();
+      audioTurn.play();
+      checkWin();
+      if (!isgameover) {
+        document.getElementsByClassName("info")[0].innerText =
+          "Turn for " + turn;
+      }
+    }
+  });
+});
+
+// Add onclick listener to reset button
+reset.addEventListener("click", () => {
+  let boxtexts = document.querySelectorAll(".boxtext");
+  Array.from(boxtexts).forEach((element) => {
+    element.innerText = "";
+  });
+  turn = "X";
+  isgameover = false;
+  document.querySelector(".line").style.width = "0vw";
+  document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+  document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width =
+    "0px";
+});
