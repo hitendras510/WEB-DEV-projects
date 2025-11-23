6bb87d0373f08a731f24e5f3dafd230f74db635f
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-23 17:02:56 +0530

    new

---
+++ TicTacToe - Game/script.js
@@ -0,0 +1,37 @@
+console.log("Welcome to Tic Tac Toe")
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "X"
+let isgameover = false;
+
+//func to change the turn
+const changeTurn = () =>{
+    return turn === "X"? "0": "X"
+}
+
+
+//func to check for a win
+const checkWin = () => {
+    let boxtext = document.getElementsByClassName('boxtext');
+ let wins = [
+   [0, 1, 2, 5, 5, 0],
+   [3, 4, 5, 5, 15, 0],
+   [6, 7, 8, 5, 25, 0],
+   [0, 3, 6, -5, 15, 90],
+   [1, 4, 7, 5, 15, 90],
+   [2, 5, 8, 15, 15, 90],
+   [0, 4, 8, 5, 15, 45],
+   [2, 4, 6, 5, 15, 135],
+ ]
+ wins.forEach(e => {
+    if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")){
+        document.querySelector('.info').innerText = boxtext[e[0]].innerText + "WON"
+        isgameover = true
+        document.querySelector('.imgbox').getElementsByTagName('.img')[0].computedStyleMap.width
+ = "200px";
+document.querySelector(".line")
+}
+ })
+
+}
