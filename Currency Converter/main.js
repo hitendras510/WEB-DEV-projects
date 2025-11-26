e80ac176992a9d27f12920b34ac3cdb91f7b4d4f
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-26 17:52:46 +0530

    . 

--- Currency Converter/main.js
+++ Currency Converter/main.js
@@ -1,1 +1,23 @@
-url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_cRZKbTzcoZYkXRxqMFDkVIaomYUMUyTKKTC4zcMY";
+
+const populate = (base, value) =>{
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_cRZKbTzcoZYkXRxqMFDkVIaomYUMUyTKKTC4zcMY&base_currency=";
+}
+
+
+
+const btn = document.querySelector(".btn")
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("button is clicked");
+    const value = document.querySelector("input[name='quantity']").value;
+    populate();
+})
+
+const tableBody = document.getElementsByTagName("tbody");
+tableBody.innerHtml = `
+<tr>
+    <td>INR</td>
+    <td>USD</td>
+    <td>EUR</td>
+</tr>
+`;
