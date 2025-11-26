2ea8f4b622aefaea1d18ce266e2f221daf5a1249
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-26 18:05:30 +0530

    . 

--- Currency Converter/main.js
+++ Currency Converter/main.js
@@ -1,23 +1,41 @@
+const populate = async (value, currency) => {
+  let myStr = "";
 
-const populate = (base, value) =>{
-    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_cRZKbTzcoZYkXRxqMFDkVIaomYUMUyTKKTC4zcMY&base_currency=";
-}
+  const url =
+    "https://api.currencyapi.com/v3/latest?apikey=cur_live_cRZKbTzcoZYkXRxqMFDkVIaomYUMUyTKKTC4zcMY&base_currency=" +
+    currency;
 
+  const response = await fetch(url);
+  let rJson = await response.json();
 
+  console.log(rJson);
 
-const btn = document.querySelector(".btn")
+  for (let key in rJson.data) {
+    const code = rJson.data[key].code;
+    const rate = rJson.data[key].value;
+    const converted = (rate * value).toFixed(2);
+
+    myStr += `
+            <tr>
+                <td>${code}</td>
+                <td>${rate}</td>
+                <td>${converted}</td>
+            </tr>
+        `;
+  }
+
+  const tableBody = document.querySelector("tbody");
+  tableBody.innerHTML = myStr;
+};
+
+const btn = document.querySelector(".btn");
 btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("button is clicked");
-    const value = document.querySelector("input[name='quantity']").value;
-    populate();
-})
+  e.preventDefault();
 
-const tableBody = document.getElementsByTagName("tbody");
-tableBody.innerHtml = `
-<tr>
-    <td>INR</td>
-    <td>USD</td>
-    <td>EUR</td>
-</tr>
-`;
+  const value = parseInt(
+    document.querySelector("input[name='quantity']").value
+  );
+  const currency = document.querySelector("select[name='currency']").value;
+
+  populate(value, currency);
+});
